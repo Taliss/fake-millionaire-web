86d248f6bf2a9b82e2a84463cef800f200340e3c
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-22 22:15:42 +0300

    WIP: better message when no data

--- src/components/ProfitInfo/BuySellInfo.js
+++ src/components/ProfitInfo/BuySellInfo.js
@@ -3,11 +3,15 @@ const { Text } = Typography;
 
 export const BuySellInfo = ({ className, type, dateTimePoint }) => {
   const displayText = type === 'buy' ? 'Buy at:' : 'Sell at:';
+
   return (
     <div className={className}>
       <Space direction="horizontal" size={40}>
         <Text className="profitInfoText">
-          {displayText} {dateTimePoint.dateTime}
+          {displayText}{' '}
+          {dateTimePoint.dateTime
+            ? dateTimePoint.dateTime
+            : '-missing date time info-'}
         </Text>
         <Text className="profitInfoText">Price at: {dateTimePoint.price}</Text>
       </Space>
