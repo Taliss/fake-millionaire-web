8b2adb7a0fb9f4a586e04ec4b6e0a29f6850f2bc
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-23 22:05:24 +0300

    fix bug and adjust error col-span

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -2,15 +2,15 @@ import { ProfitInfo } from './ProfitInfo';
 import { Space, Row, Col, Typography } from 'antd';
 import { useSelector } from 'react-redux';
 import { getBuySellPointsErrorStatus } from '../store/millionaire';
-const { Text } = Typography;
+const { Paragraph } = Typography;
 
 export const ContentSection = () => {
   const errorWhileFetching = useSelector(getBuySellPointsErrorStatus);
   return (
     <Row className="gutter-row">
-      <Col span={errorWhileFetching ? 16 : 23}>
+      <Col span={errorWhileFetching ? 12 : 23}>
         {errorWhileFetching ? (
-          <Text type="danger">{errorWhileFetching}</Text>
+          <Paragraph type="danger">{errorWhileFetching}</Paragraph>
         ) : (
           <ProfitInfo />
         )}
