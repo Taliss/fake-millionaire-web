0e69d20a47f4ce9edfe0c02f4617a0d41d838349
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-23 19:59:32 +0300

    format date with moment

--- src/components/ProfitInfo/BuySellInfo.js
+++ src/components/ProfitInfo/BuySellInfo.js
@@ -1,16 +1,21 @@
 import { Typography, Space } from 'antd';
+import moment from 'moment';
 const { Text } = Typography;
 
-export const BuySellInfo = ({ className, type, dateTimePoint }) => {
+export const BuySellInfo = ({
+  className,
+  type,
+  dateTimePoint,
+  dateFormatter = 'MMMM Do YYYY, h:mm:ss',
+}) => {
   const displayText = type === 'buy' ? 'Buy at:' : 'Sell at:';
-
   return (
     <div className={className}>
       <Space direction="horizontal" size={40}>
         <Text className="profitInfoText">
           {displayText}{' '}
           {dateTimePoint.dateTime
-            ? dateTimePoint.dateTime
+            ? moment(dateTimePoint.dateTime).format(dateFormatter)
             : '-missing date time info-'}
         </Text>
         <Text className="profitInfoText">Price at: {dateTimePoint.price}</Text>
